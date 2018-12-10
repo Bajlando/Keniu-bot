5632a09efc868214f50575b1e2ea0a8ccf2b5ad7
Bajlando <45641434+Bajlando@users.noreply.github.com>
2018-12-10 17:29:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,58 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+	let fontSize = 90;
+
+	do {
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	return ctx.font;
+};
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.find(ch => ch.name === '✋-powitalnia-✋');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(786, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '40px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Witaj na serwerze,', canvas.width / 2.5, canvas.height / 3.5);
+
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.7);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
+	const avatar = await Canvas.loadImage(buffer);
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`**${member}** **Właśnie dołączył na serwer! Zerknij do regulaminu i baw się dobrze**`, attachment);
+});
+
+client.on('message', async message => {
+	if (message.content === '.join') {
+		client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+	}
+});
+
 client.on('ready', () => {
     let autopisanko = "10s";
     setInterval(function() {
