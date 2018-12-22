035eb4a83f7d1c1828700747363019de25da6f38
Bajlando <45641434+Bajlando@users.noreply.github.com>
2018-12-22 22:31:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ".";
 const fs = require("fs");
 const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
