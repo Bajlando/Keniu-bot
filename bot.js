1a8975b48fb65a81ccaae31b1efc64ceeba7ef21
Bajlando <45641434+Bajlando@users.noreply.github.com>
2018-12-22 22:21:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,54 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "$";
+const prefix = ".";
 const fs = require("fs");
 const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
+const disco = new Discord.Client();
+const allowedUsers = "263340222375264257"
+const roles = "Test"
+
+client.on("ready", () => {
+    client.user.setGame(`k.help | Na ${client.guilds.size} serwerach <3`, "https://www.twitch.tv/vami12");
+});
+
+client.on("ready", () => {
+    client.user.setStatus("dnd");
+});
+
+
+disco.on("message", message => {
+
+  function discoRole() {
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    roles.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      theRole.edit({color: random}).catch(e => {
+        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
+      });
+    });
+  }
+
+  if(message.content.startsWith(prefix + "startdisco")) {
+    if(allowedUsers.includes(message.author.id)) {
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.send("```css\nDiscoing...```");
+    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
+  } else {
+    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+} else
+
+if(message.content.startsWith(prefix + "stopdisco")) {
+  if(allowedUsers.includes(message.author.id)) {
+  message.channel.send("I've stopped discoing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
+  }
+}
+
+});
 
 const serverStats = {
     guildID: '303657307231879169',
