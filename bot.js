e5623fe5ba42393783cf30e02775e0b65a16b15f
Bajlando <45641434+Bajlando@users.noreply.github.com>
2019-01-08 20:48:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,41 +118,6 @@ const applyText = (canvas, text) => {
 	return ctx.font;
 };
 
-client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find(ch => ch.name === '✋-powitalnia-✋');
-	if (!channel) return;
-
-	const canvas = Canvas.createCanvas(786, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('./wallpaper.jpg');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	ctx.font = '40px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Witaj na serwerze,', canvas.width / 2.5, canvas.height / 3.5);
-
-	ctx.font = applyText(canvas, `${member.displayName}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.7);
-
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-
-	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
-	const avatar = await Canvas.loadImage(buffer);
-	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-	channel.send(`**${member}** **Właśnie dołączył na serwer! Zerknij do regulaminu i baw się dobrze**`, attachment);
-});
-
 client.on('ready', () => {
     let autopisanko = "10s";
     setInterval(function() {
@@ -236,11 +201,16 @@ client.on('message', async message => {
 
 if (command == "help") {
 
-  let sklep1 = new Discord.RichEmbed()
-  .setTitle("k.disco")
-  .setColor(12248579)
+  let help = new Discord.RichEmbed()
+  .setAuthor("Keniu Bot", "https://cdn.discordapp.com/avatars/519988098789277696/4d0e77ded3750f51ba218af0592dfed4.png?size=2048")
+  .setColor('#9900cc')
+  .setTitle("Pomocne komendy, prefix k.")
+  .addField("**Design **", "`disco`, `animacja`")
+  .addField("**Moderacyjne**", "`warn`, `mute`, `ban`, `kick`, `clear`")
+  .addField("**4 fun**", "`cat`, `dog`, `random`, `rip`, `hug`, `kiss`, `punch`, `slap`, `papieros`, `ziemniak`, `kill`, `sex`, `8ball`, `flip`, `say`, `hacked`")
+  .addField("**Pozostałe**", "`botinfo`, `ping`, `report`, `serverinfo`, `userinfo`, `avatar`")
 
-  message.channel.send(sklep1);
+  message.channel.send(help);
 
 }});
 
@@ -251,25 +221,40 @@ client.on('message', async message => {
 
 if (command == "disco") {
 
-  let sklep = new Discord.RichEmbed()
-  .setTitle("Disco")
+  let disco = new Discord.RichEmbed()
+  .setTitle("**Disco Rola**")
   .setColor(12248579)
   .addField("(Zapamiętaj że ranga Keniu Bot musi być wyżej niż rola Disco)", "Aby ustawić Rolę disco wpisz k.discoustaw")
+	.setThumbnail("https://cdn.discordapp.com/attachments/497774400515342336/531918274619899935/unknown.png")
+	.setImage("https://cdn.discordapp.com/attachments/497774400515342336/531917417581117451/ezgif-2-8430ce96d061.gif")
 
-  message.channel.send(sklep);
+  message.channel.send(disco);
 
 }});
 
 client.on('message', async msg => {
     if (msg.content.toLowerCase() === prefix + "discoustaw") {
    if (msg.channel.type === "dm") return;
-   msg.guild.createRole({
-        name: "Disco",
-        color: "#000000",
-        permissions:[]
-      })
+	 let discorola = msg.guild.roles.find(`name`, "Disco");
+   if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply(":potato: **Nie masz uprawnień do stworzenia disco roli :)** :potato:");
+   if(!discorola){
+     try{
+       discorola = await msg.guild.createRole({
+         name: "Disco",
+         color: "#000000",
+         permissions:[]
+       })
+       msg.guild.channels.forEach(async (channel, id) => {
+         await channel.overwritePermissions(discorola, {
+           SEND_MESSAGES: true,
+           ADD_REACTIONS: true
+         });
+       });
+     }catch(e){
+       console.log(e.stack);
+     }
+   }
    msg.channel.send("**Ranga Disco pomyślnie ustawiona**");
-  const rol = 'disco'
   setInterval(() => {
       msg.guild.roles.find('name', 'Disco').edit({color: 'RANDOM'})
     }, 3500);
@@ -281,6 +266,18 @@ client.on('guildCreate', (guild) => {
   const embed = new Discord.RichEmbed()
     .setTitle('Dodano bota')
     .setColor('#00ff0c')
+    .addField('Nazwa serwera:', `${guild.name}`)
+    .addField('Id serwera:', `${guild.id}`)
+    .addField('Właściciel serwera:', `${guild.owner.user.username}#${guild.owner.user.discriminator} (${guild.owner.user.id})`)
+    .addField('Ludzi', `${guild.memberCount}`)
+  client.channels.get(channel.id).sendEmbed(embed)
+});
+
+client.on('guildDelete', (guild) => {
+  let channel = client.channels.get('497774400515342336')
+  const embed = new Discord.RichEmbed()
+    .setTitle('Usunięto bota')
+    .setColor('#ff0000')
     .addField('Nazwa serwera:', `${guild.name}`)
     .addField('Id serwera:', `${guild.id}`)
     .addField('Właściciel serwera:', `${guild.owner.user.username}#${guild.owner.user.discriminator} (${guild.owner.user.id})`)
