b04b69da2e111b6c2ebc8f5459f915bf55a192e3
Bajlando <45641434+Bajlando@users.noreply.github.com>
2019-04-29 17:12:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,37 +5,16 @@ const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
 const prefix = "r.";
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('500759364181950464').roles.find('name', 'Disco').edit({color: 'RANDOM'})
+    },3500);
+  });
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (message.content.startsWith(" ")) {
-    if (message.author.bot) return;
-      if(!message.channel.guild) return message.reply('ta');
-    let say = new Discord.RichEmbed()
-  .setTitle("Od " + message.author.username)
-  .setThumbnail(message.author.avatarURL)
-  .setAuthor("PROPOZYCJA")
-    .setDescription(args.join("  "))
-    .setColor(12248579)
-      .setFooter(" ")
-    message.guild.channels.find('name','propozycje').send(say).then(embedMessage => {
-        embedMessage.react("👍");
-        embedMessage.react("👎");
-    });
-    message.author.send("**- Dziękujemy za oddanie propozycji** :heart:");
-
-    message.delete();
-  }
-
-
-});
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('348639545530515466').roles.find('name', 'Disco').edit({color: 'RANDOM'})
+    },3500);
+  });
 
 client.login(process.env.BOT_TOKEN);
