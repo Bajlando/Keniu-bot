38b3b45a42138dc126906e4d85bcb6dcb512e981
Bajlando <45641434+Bajlando@users.noreply.github.com>
2019-04-29 17:28:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,8 @@
-const Discord = require('discord.js');
-const client = new Discord.Client({fetchAllMembers: true});
-const fs = require("fs");
+const        Discord = require('discord.js');
+const         client = new Discord.Client({fetchAllMembers: true});
 const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
-const prefix = "r.";
-
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('500759364181950464').roles.find('name', 'Disco').edit({color: 'RANDOM'})
-    },3500);
-  });
+const prefix = ".";
 
 client.on('ready', () => {
     setInterval(function(){
